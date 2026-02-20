81d329436b055ced53cb5839e268e65430805c43
pinagof <pinagof@ieselaios.com>
2026-02-20 09:53:27 +0000

    update franweird

--- franweird.cpp
+++ franweird.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout << "Dame un numero entero posivivo ";
-    cin >> n;
-
-    if {n%2==1
-    n=impar}
-
+#define ll long long
+void weird(ll n){
+while(n!= 1){
+        if(n%2 == 0){
+            n=n/2;
+        } else {
+            n = n* 3 + 1;
+        }
+        cout << n << " ";
+    }
 }
+int main(){
+    ll n;
+    cout << "Dame un numero entero positivo ";
+    cin >> n;
+    cout << n << "\n";
+    weird(n);
+    cout << endl;
+}
+
